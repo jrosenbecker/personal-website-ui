6eda75b807cad741c0ac75f6255a95ae53f9c78f
jrosenbecker <rosenbeckerjw>
2018-07-14 07:17:42 -0500

    Forcing SSL

--- server.js
+++ server.js
@@ -4,6 +4,26 @@ const path = require('path');
 
 const app = express();
 
+const port = process.env.PORT || 8080;
+
+const forceSSL = function() {
+    return function (req, res, next) {
+        if (req.headers['x-forwarded-proto'] !== 'https') {
+        return res.redirect(
+            ['https://', req.get('Host'), req.url].join('')
+        );
+        }
+        next();
+    }
+}
+// Instruct the app
+// to use the forceSSL
+// middleware
+if (process.env.ENVIRONMENT === 'PROD') {
+    console.log('Forcing SSL');
+    app.use(forceSSL());
+}
+
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
@@ -13,4 +33,6 @@ res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
